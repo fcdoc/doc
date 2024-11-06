27c260ae06149d0a69bc66ca06c7ca0bc9106772
i18n <i18n.site@gmail.com>
2024-11-06 23:25:52 +0800

    https://i18n.site

---
+++ dist.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+DIR=$(realpath $0) && DIR=${DIR%/*}
+cd $DIR
+set -ex
+
+git add .
+git commit -mdist || true
+git fetch --unshallow 2>/dev/null || true
+git pull
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+merge() {
+  if [[ "$branch" != "$1" ]]; then
+    git fetch origin $1
+    git merge origin/$1 --no-edit
+    git add . && git commit -m"merge $1" || true
+  fi
+}
+
+merge main
+merge dist
+
+git push || true
+
+git push origin $branch:dist
