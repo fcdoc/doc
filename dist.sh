92d8c905ea195aa15e8d70f3f204b25603161c2a
i18n <i18n.site@gmail.com>
2024-11-06 23:22:26 +0800

    dist

--- dist.sh
+++ dist.sh
@@ -6,7 +6,22 @@ set -ex
 
 git add .
 git commit -mdist || true
-git pull origin main
-git pull origin dist
+git fetch --unshallow
+git pull
+
+merge() {
+  if [[ "$branch" != "$1" ]]; then
+    git fetch origin $1
+    git merge origin/$1 --no-edit
+    git add . && git commit -m"merge $1" || true
+  fi
+}
+
+merge main
+merge dist
+
+git push || true
+
 branch=$(git rev-parse --abbrev-ref HEAD)
+
 git push origin $branch:dist
