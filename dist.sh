806345445c927df778db223d92a8ea00329c3d11
i18n <i18n.site@gmail.com>
2024-11-07 13:41:12 +0800

    ♨

--- dist.sh
+++ dist.sh
@@ -15,7 +15,8 @@ merge() {
   if [[ "$branch" != "$1" ]]; then
     git fetch origin $1
     git merge origin/$1 --no-edit
-    git add . && git commit -m"merge $1" && git push origin $branch:$1 || true
+    git add . && git commit -m"merge $1" || true
+    git push origin $branch:$1 || true
   fi
 }
 
